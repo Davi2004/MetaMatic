c21c566e4034cb21038c6eb96fa50248bc7226a8
Davimrp <davipereirar39@gmail.com>
2024-09-12 10:00:05 -0300

    UPDATE .gitignore

--- JavaScript base/Variáveis.js
+++
@@ -1,9 +0,0 @@
-/* Declaração de variáveis e retorno ao usuário: 
-
-    // Hello, Word! ou Olá, mundo! (Piada interna entre os programadores) <-- Exemplo de comentário
-
-    let mensagem = "Utilizando variáveis com o Maykão da RocketSeat" 
-    
-    console.log(mensagem);
-
-*/
